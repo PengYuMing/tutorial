9ad813c70aaf5e1ad9835654c3dc1485a081a8eb
NickPeng <nick6260@gmail.com>
2015-06-22 18:08:29 +0800

    Initial Commit

---
+++ study/buttontest/buttontest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  buttontest
+//
+//  Created by 彭煜銘 on 2015/6/22.
+//  Copyright (c) 2015年 NickPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ study/buttontest/buttontest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  buttontest
+//
+//  Created by 彭煜銘 on 2015/6/22.
+//  Copyright (c) 2015年 NickPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
